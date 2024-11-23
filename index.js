cc8de6924e2d093d199536c7bcf9bcb6d451af4b
lydiassssz <mmy.tanpopo@gmail.com>
2024-11-23 15:25:21 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client(
+  { intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+    ]
+  }
+);
+
+client.on('messageCreate', message => {
+  if(message.author.bot) return; //BOTのメッセージには反応しない
+
+  if(message.content === "bot test") {
+    message.channel.send("Hello World!");
+  }
+});
+
+require('dotenv').config({ path: '.env' });
+
+client.on('ready', () => {
+    console.log('bot is ready');
+});
+
+client.login(process.env.DISCORD_TOKEN);
