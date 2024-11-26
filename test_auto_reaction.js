71a8404fa9c83eab59e07b53881b5bd9f1692a47
sige <sige>
2024-11-26 17:15:21 +0900

    リアクションがなくなった時に通知する機能を追加

--- test_auto_reaction.js
+++ test_auto_reaction.js
@@ -21,7 +21,7 @@ client.on("messageCreate", async (message) => {
   const announcementChannelId = "1309759525603115138"; // チャンネルIDを指定
 
   // Botのメッセージにはリアクションしない
-  if (user.bot) return;
+  if (message.author.bot) return;
 
   // 指定したIDのチャンネルメッセージにリアクションを追加する
   if (message.channel.id === announcementChannelId) {
@@ -45,7 +45,24 @@ client.on("messageReactionAdd", async (reaction, user) => {
     // リアクションが追加されたことを指定のチャンネルに呟く
     const channel = reaction.message.channel;
     await channel.send(
-      `「 ${user.username}」がリアクション「${emojiName}」をつけました！`
+      `「${user.username}」がリアクション「${emojiName}」をつけました！`
+    );
+  }
+});
+
+// リアクションがなくなったときの処理
+client.on("messageReactionRemove", async (reaction, user) => {
+  const announcementChannelId = "1309759525603115138";
+
+  if (user.bot) return;
+
+  if (reaction.message.channel.id === announcementChannelId) {
+    const emojiName = reaction.emoji.name;
+
+    // リアクションが削除されたことを指定のチャンネルに呟く
+    const channel = reaction.message.channel;
+    await channel.send(
+      `「${user.username}」がリアクション「${emojiName}」を外しました！`
     );
   }
 });
